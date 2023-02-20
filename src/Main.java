6e2826277c5ab82dc5ea8367f6ab6f4a5f798739
action0709 <action0709@mail.ru>
2023-02-20 19:52:15 +0400

    дз 1,2,3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        task1();
+        task2();
+        task3();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        // Пишем код для задачи 1
+        byte age = 15;
+        if (age >= 18) {
+            System.out.println("Человеку 18 или больше лет");
+        } else {
+            System.out.println("Возраст совершеннолетия еще не наступил и нужно немного подождать");
+        } ;
+    }
+
+     public static void task2 () {
+         System.out.println("Задача 2");
+         // Пишем код для задачи 2
+         byte temperature = -15;
+         if (temperature>= -5) {
+             System.out.println("Сегодня тепло, можно идти без шапки");
+         } else {
+             System.out.println("На улице холодно, нужно надеть шапку");
+         }
+     }
+    public static void task3 () {
+        System.out.println("Задача 3");
+        // Пишем код для задачи 3
+        byte speed = 80;
+        if (speed>= 60) {
+            System.out.println("Если скорость "+speed+", то придется заплатить штраф");
+        } else {
+            System.out.println("Если скорость "+speed+", то можно ездить спокойно");
+        }
     }
 }
